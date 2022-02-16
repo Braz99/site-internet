b7f258a9db1b7bfe6ba09af22afb489b3d750600
braz99 <fabriciobrazil99@gmail.com>
2022-02-16 11:23:30 -0300

    Coloca uma fonte e ajusta o css do testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,7 +3,12 @@ import styles from "../../styles/components/testimonial.module.css";
 export default function Testimonial({ children, name, photo }) {
   return (
     <section className={styles.testimonial}>
-      <div className={styles.photo}>{photo ? photo : "Foto"}</div>
+      <img
+        className={styles.photo}
+        alt="foto-cliente"
+        src={photo ? photo : "favicon.ico"}
+      />
+
       <div className={styles.group}>
         <h3 className={styles.name}>{name}</h3>
         <p className={styles.text}>{children}</p>
